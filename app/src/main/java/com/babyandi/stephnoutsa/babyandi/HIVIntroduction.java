798994bdb69fe89c78f77de24ed9946c29283752
Steph Noutsa <stephanenoutsa@yahoo.com>
2016-02-18 00:47:52 +0100

    Adds text to the HIVIntroduction activity and makes it scrollable

--- app/src/main/java/com/babyandi/stephnoutsa/babyandi/HIVIntroduction.java
+++ app/src/main/java/com/babyandi/stephnoutsa/babyandi/HIVIntroduction.java
@@ -6,9 +6,11 @@ import android.os.Bundle;
 //import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import static com.babyandi.stephnoutsa.babyandi.R.drawable.android;
 
@@ -33,6 +35,8 @@ public class HIVIntroduction extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });*/
+        TextView hivIntroduction = (TextView) findViewById(R.id.hivIntroduction);
+        hivIntroduction.setMovementMethod(new ScrollingMovementMethod());
     }
 
     ////////////Intents for menu items////////////
