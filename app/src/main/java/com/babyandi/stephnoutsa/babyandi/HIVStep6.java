a888840539a872aaa42eff7295e225a6e1932fe9
Steph Noutsa <stephanenoutsa@yahoo.com>
2016-02-18 02:04:43 +0100

    Adds text to the HIVStep6 activity and makes it scrollable.

--- app/src/main/java/com/babyandi/stephnoutsa/babyandi/HIVStep6.java
+++ app/src/main/java/com/babyandi/stephnoutsa/babyandi/HIVStep6.java
@@ -6,9 +6,11 @@ import android.os.Bundle;
 //import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import static com.babyandi.stephnoutsa.babyandi.R.drawable.android;
 
@@ -33,6 +35,9 @@ public class HIVStep6 extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });*/
+
+        TextView hivStep6 = (TextView) findViewById(R.id.hivStep6);
+        hivStep6.setMovementMethod(new ScrollingMovementMethod());
     }
 
     ////////////Intents for menu items////////////
