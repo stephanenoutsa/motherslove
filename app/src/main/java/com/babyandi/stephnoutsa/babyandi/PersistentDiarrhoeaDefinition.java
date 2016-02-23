8f11a159a1c9d71b36fe177b1669e0be58854b68
Steph Noutsa <stephanenoutsa@yahoo.com>
2016-02-23 15:22:29 +0100

    Adds the literature for the PersistentDiarrhoeaDefinition activity and makes it scrollable.

--- app/src/main/java/com/babyandi/stephnoutsa/babyandi/PersistentDiarrhoeaDefinition.java
+++ app/src/main/java/com/babyandi/stephnoutsa/babyandi/PersistentDiarrhoeaDefinition.java
@@ -6,9 +6,11 @@ import android.os.Bundle;
 //import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import static com.babyandi.stephnoutsa.babyandi.R.drawable.android;
 
@@ -33,6 +35,9 @@ public class PersistentDiarrhoeaDefinition extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });*/
+
+        TextView persistentDiarrhoeaDefinition = (TextView) findViewById(R.id.persistentDiarrhoeaDefinition);
+        persistentDiarrhoeaDefinition.setMovementMethod(new ScrollingMovementMethod());
     }
 
     ////////////Intents for menu items////////////
