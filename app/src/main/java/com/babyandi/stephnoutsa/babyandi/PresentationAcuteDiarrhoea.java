b47737efd3ab0cefbaba838de588bdc754119611
Steph Noutsa <stephanenoutsa@yahoo.com>
2016-02-23 13:19:48 +0100

    Adds the literature for the PresentationAcuteDiarrhoea activity and makes it scrollable.

--- app/src/main/java/com/babyandi/stephnoutsa/babyandi/PresentationAcuteDiarrhoea.java
+++ app/src/main/java/com/babyandi/stephnoutsa/babyandi/PresentationAcuteDiarrhoea.java
@@ -6,9 +6,11 @@ import android.os.Bundle;
 //import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import static com.babyandi.stephnoutsa.babyandi.R.drawable.android;
 
@@ -33,6 +35,9 @@ public class PresentationAcuteDiarrhoea extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });*/
+
+        TextView acuteDiarrhoeaPresentation = (TextView) findViewById(R.id.acuteDiarrhoeaPresentation);
+        acuteDiarrhoeaPresentation.setMovementMethod(new ScrollingMovementMethod());
     }
 
     ////////////Intents for menu items////////////
