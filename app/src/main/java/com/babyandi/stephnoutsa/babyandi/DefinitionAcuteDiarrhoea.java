afba9339142c2a27f13a3b7db92884678f7b6e77
Steph Noutsa <stephanenoutsa@yahoo.com>
2016-02-23 12:54:18 +0100

    Adds the literature for the DefinitionAcuteDiarrhoea activity and its associated image

--- app/src/main/java/com/babyandi/stephnoutsa/babyandi/DefinitionAcuteDiarrhoea.java
+++ app/src/main/java/com/babyandi/stephnoutsa/babyandi/DefinitionAcuteDiarrhoea.java
@@ -6,9 +6,11 @@ import android.os.Bundle;
 //import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import static com.babyandi.stephnoutsa.babyandi.R.drawable.android;
 
@@ -33,6 +35,9 @@ public class DefinitionAcuteDiarrhoea extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });*/
+
+        TextView acuteDiarrhoeaDefinition = (TextView) findViewById(R.id.acuteDiarrhoeaDefinition);
+        acuteDiarrhoeaDefinition.setMovementMethod(new ScrollingMovementMethod());
     }
 
     ////////////Intents for menu items////////////
