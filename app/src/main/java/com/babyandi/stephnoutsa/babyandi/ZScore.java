4daafe452c1fdd5486da70baf50e46de0630ecc6
Steph Noutsa <stephanenoutsa@yahoo.com>
2016-02-25 03:55:43 +0100

    Adds intents for the menu items in the ZScore activity.

--- app/src/main/java/com/babyandi/stephnoutsa/babyandi/ZScore.java
+++ app/src/main/java/com/babyandi/stephnoutsa/babyandi/ZScore.java
@@ -1,10 +1,13 @@
 package com.babyandi.stephnoutsa.babyandi;
 
+import android.content.Intent;
 import android.os.Bundle;
 //import android.support.design.widget.FloatingActionButton;
 //import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -755,6 +758,75 @@ public class ZScore extends AppCompatActivity {
                 zScore.setText(R.string.zscore_malnourished);
             }
         }
+    }
+
+    ////////////Intents for menu items////////////
+    public void onClickAnc() {
+        Intent i = new Intent(this, ANC.class);
+        startActivity(i);
+    }
+
+    public void onClickMalnutrition() {
+        Intent i = new Intent(this, Malnutrition.class);
+        startActivity(i);
+    }
+
+    public void onClickDiarrhoea() {
+        Intent i = new Intent(this, Diarrhoea.class);
+        startActivity(i);
+    }
+
+    public void onClickSpecialNeeds() {
+        Intent i = new Intent(this, SpecialNeeds.class);
+        startActivity(i);
+    }
+    ////////////////End of Intents/////////////////
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_activities, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        /*if (id == R.id.search) {
+            onSearchRequested();
+            return true;
+        }*/
+        if (id == R.id.notifications) {
+            Intent i = new Intent(this, Notifications.class);
+            startActivity(i);
+        }
+        if (id == R.id.about) {
+            Intent i = new Intent(this, About.class);
+            startActivity(i);
+        }
+        if (id == R.id.go_to_anc) {
+            onClickAnc();
+            return true;
+        }
+        if (id == R.id.go_to_malnutrition) {
+            onClickMalnutrition();
+            return true;
+        }
+        if (id == R.id.go_to_diarrhoea) {
+            onClickDiarrhoea();
+            return true;
+        }
+        if (id == R.id.go_to_special_needs) {
+            onClickSpecialNeeds();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
