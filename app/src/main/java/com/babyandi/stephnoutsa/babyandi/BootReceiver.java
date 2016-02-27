0d3fe36e2d52ce3fe6de7387cd5af91ccd76669a
Steph Noutsa <stephanenoutsa@yahoo.com>
2016-02-27 14:32:25 +0100

    Adds the BootReceiver class to start a service when the phone boots

---
+++ app/src/main/java/com/babyandi/stephnoutsa/babyandi/BootReceiver.java
@@ -0,0 +1,22 @@
+package com.babyandi.stephnoutsa.babyandi;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if(intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
+            Intent service = new Intent(context, MyService.class);
+            PendingIntent pendingIntent = PendingIntent.getService(context, 0, service, 0);
+
+            AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+            alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                    60 * 1000, 60 * 1000,
+                    pendingIntent);
+        }
+    }
+}
