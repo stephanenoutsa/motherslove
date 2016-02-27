a45792df1fb2bdb935b72a7f15873f16f89aac1d
Steph Noutsa <stephanenoutsa@yahoo.com>
2016-02-27 15:35:35 +0100

    Adds the logic to save the lmp and calculate the expected date of delivery

--- app/src/main/java/com/babyandi/stephnoutsa/babyandi/WhenYoullDeliver.java
+++ app/src/main/java/com/babyandi/stephnoutsa/babyandi/WhenYoullDeliver.java
@@ -1,6 +1,11 @@
 package com.babyandi.stephnoutsa.babyandi;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 //import android.support.design.widget.FloatingActionButton;
 //import android.support.design.widget.Snackbar;
@@ -9,8 +14,21 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class WhenYoullDeliver extends AppCompatActivity {
+
+    MyDBHandler dbHandler;
+    TextView eDD;
+    String dbDate;
+    int yr, mth, day;
+    public GregorianCalendar selectedDate = new GregorianCalendar();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +49,66 @@ public class WhenYoullDeliver extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });*/
+
+        dbHandler = new MyDBHandler(this, null, null, 1);
+        eDD = (TextView) findViewById(R.id.eDD);
+
+        CalendarView date = (CalendarView) findViewById(R.id.calendarView);
+
+        // Set maximum date to be displayed
+        long max = date.getDate();
+        date.setMaxDate(max);
+
+        date.setOnDateChangeListener( new CalendarView.OnDateChangeListener() {
+            public GregorianCalendar calendar;
+
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                this.calendar = new GregorianCalendar(year, month, dayOfMonth);
+                yr = calendar.get(Calendar.YEAR);
+                mth = calendar.get(Calendar.MONTH);
+                day = calendar.get(Calendar.DAY_OF_MONTH);
+                selectedDate = new GregorianCalendar(yr, mth, day);
+                //Put a toast on the screen for testing purposes
+                //Toast.makeText(getApplicationContext(), calendar + "/" + mth + "/" + year, Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    // Function to retrieve selected date
+    public void onClickSubmitDate(View view) {
+        DateFormat sdf = new SimpleDateFormat("MMM d, yyyy");
+        String date = sdf.format(selectedDate.getTime());
+        LMP lmp = new LMP(date);
+        dbHandler.addLMP(lmp);
+
+        // Calculate expected date of delivery
+        selectedDate.add(Calendar.MONTH, -3);
+        selectedDate.add(Calendar.DAY_OF_MONTH, 7);
+        selectedDate.add(Calendar.YEAR, 1);
+        dbDate = sdf.format(selectedDate.getTime());
+
+        // Display expected date of delivery
+        eDD.setText(dbDate);
+
+        // Start service
+        Intent service = new Intent(this, MyService.class);
+        PendingIntent pendingIntent = PendingIntent.getService(this, 0, service, 0);
+
+        AlarmManager alarmManager = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
+        alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                0, 1000 * 60 * 60 * 24,
+                pendingIntent);
+
+        // Enable receiver when device boots
+        ComponentName receiver = new ComponentName(this, BootReceiver.class);
+        PackageManager pm = this.getPackageManager();
+
+        pm.setComponentEnabledSetting(receiver,
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                PackageManager.DONT_KILL_APP);
+
+
     }
 
     ////////////Intents for menu items////////////
