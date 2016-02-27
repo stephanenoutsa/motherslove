47f91c910c21450a3f85b129b64fab810096a39d
Steph Noutsa <stephanenoutsa@yahoo.com>
2016-02-27 15:37:22 +0100

    Starts service 1 minute after phone has booted, then resumes it 1 day later

--- app/src/main/java/com/babyandi/stephnoutsa/babyandi/BootReceiver.java
+++ app/src/main/java/com/babyandi/stephnoutsa/babyandi/BootReceiver.java
@@ -15,7 +15,7 @@ public class BootReceiver extends BroadcastReceiver {
 
             AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
             alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                    60 * 1000, 60 * 1000,
+                    60 * 1000, 1000 * 60 * 60 * 24,
                     pendingIntent);
         }
     }
