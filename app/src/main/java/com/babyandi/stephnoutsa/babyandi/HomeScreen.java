b66caf2be3f156d5791ceafc51c38f1e96df9fbf
Steph Noutsa <stephanenoutsa@yahoo.com>
2016-03-23 01:41:45 +0100

    Makes the HomeScreen activity to use the menu_main.xml file to display its menu

--- app/src/main/java/com/babyandi/stephnoutsa/babyandi/HomeScreen.java
+++ app/src/main/java/com/babyandi/stephnoutsa/babyandi/HomeScreen.java
@@ -80,7 +80,7 @@ public class HomeScreen extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
