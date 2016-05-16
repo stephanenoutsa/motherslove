eb9b72b17e4b1d392e5bc850e48c1f2a8010a748
Steph Noutsa <stephanenoutsa@yahoo.com>
2016-05-16 21:08:28 +0100

    Creates a receiver for the Infant Immunization Schedule

---
+++ app/src/main/java/com/babyandi/stephnoutsa/babyandi/MyImmunizationReceiver.java
@@ -0,0 +1,23 @@
+package com.babyandi.stephnoutsa.babyandi;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MyImmunizationReceiver extends BroadcastReceiver {
+
+    AlarmStart alarmStart = new AlarmStart();
+
+    public MyImmunizationReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Start service
+        Intent service = new Intent(context, MyImmunizationReceiver.class);
+        context.startService(service);
+
+        //Schedule next alarm
+        alarmStart.startImmAlarm(context);
+    }
+}
